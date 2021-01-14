6d26223cc55f788e3ec8b6a5d7309993ffe85df0
Serdar <serdar83durmus@gmail.com>
2021-01-14 21:20:39 +0100

    added pre_save

---
+++ src/blog/signals.py
@@ -0,0 +1,13 @@
+from django.db.models.signals import pre_save  # Host'u kaydetmeden önce slug oluştursun istiyorum. Bu yüzden bu metodu kullanıyoruz
+from django.dispatch import receiver  # hosttan save e basınca receiver yazılan kodun gerçekleşmesini sağlıyor
+from  django.template.defaultfilters import slugify  # slug arasındaki tireleri koyan fonksiyonu import ediyoruz
+from .models import Post
+
+@receiver(pre_save, sender=Post)
+def pre_save_create_slug(sender, instance, **kwargs):
+    if not instance.slug:
+        instance.slug = slugify(
+            instance.author.username + " " + instance.title)
+        
+# a = "serdar durmus"
+# print(slugify(a))
