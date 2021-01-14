d2416cbd39c38610ece522ad99c4e32db599f507
Serdar <serdar83durmus@gmail.com>
2021-01-15 00:03:34 +0100

    post_create er inne

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import PostForm
+
 
 # Create your views here.
 def post_list(request):
@@ -8,3 +10,19 @@ def post_list(request):
         "object_list": qs
     }
     return render(request, "blog/post_list.html", context)
+
+def post_create(request):
+    # form = PostForm(request.POST or None, request.FILES or None)
+    # veya şu 3 kod aynı işi yapıyor
+    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():  # author kısmı boş olmasın diye bu ifi yazdık Ya da kullanıcı kimse yazar da o olması gerekiyor
+            post = form.save(commit=False)  # Datayı kaydet ama database e işleme demek bu
+            post.author = request.user
+            post.save()
+            return redirect("blog:list")
+    context = {
+        'form': form
+    }
+    return render(request, "blog/post_create.html", context)
